6759ce1e57284bad6cefdbe176918182b3cf8b07
Rahul-Kumar-Paswan <rahulkumarpaswan941+auropro@gmail.com>
2023-11-26 15:29:32 +0530

    adding rds

--- AuroPro_Project_3/modules/database/outputs.tf
+++ AuroPro_Project_3/modules/database/outputs.tf
@@ -9,7 +9,3 @@ output "db_subnet_group_name" {
 output "my_rds_sg_id" {
   value = aws_security_group.my_rds_sg.id
 }
-
-output "db_instance_endpoint" {
-  value = aws_db_instance.my_db_instance.endpoint
-}
